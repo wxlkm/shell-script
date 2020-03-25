51e30da90243d4b80322049d7f44b5a59d0c6666
wxlkm <58168483+wxlkm@users.noreply.github.com>
2020-03-25 12:25:40 +0800

    Create setenv-prjk.sh

---
+++ tomcat/setenv-prjk.sh
@@ -0,0 +1,2 @@
+#鹏润金服时使用
+CATALINA_OPTS="-server -Xms2048m -Xmx4096m -Xss512K -XX:PermSize=2048m -XX:MaxPermSize=2048m -Dfile.encoding=utf-8 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Denv=pro -Didc=default -Dcom.sun.management.jmxremote.port=9999"
